6f05dfdd48f2ac7ebeb4b8779e0793b5ef629409
Taibu Haroub <haroubtaibu@gmail.com>
2021-05-22 13:07:52 +0300

    Set CORS headers on the server

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "1mb" }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/api/v1/feed", feedRoutes);
 
